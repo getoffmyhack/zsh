4a403f750ee754a392204eef81975f0ac84538a0
Justin England <justin@fbsd.flat>
2022-04-01 14:23:08 -0700

    Added simple installer.  Added dot.vimrc.
    TODO: re-arrange files, add macos files, build better intstall script.

--- BSD/install.sh
+++ BSD/install.sh
@@ -1,2 +1,9 @@
+echo "Changing shell to /usr/local/bin/zsh"
+chsh -s /usr/local/bin/zsh
+
+echo "Copying .zshrc and .oh-my-zsh/"
 cp dot.zshrc ~/.zshrc
 cp -R dot.oh-my-zsh/ ~/.oh-my-zsh
+
+echo "Copying .vimrc"
+cp dot.vimrc ~/.vimrc
